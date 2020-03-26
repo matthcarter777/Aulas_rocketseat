7636fa733e8dcbcdd2cf25dad283c58b3518d9ca
Mateus <mateushbsb@gmail.com>
2020-03-26 20:15:47 -0300

    teste

---
+++ ES6+/Modulo 2/src/main.js
@@ -0,0 +1,21 @@
+/*
+//import {soma,sub} from './funcoes';
+//import soma from './soma';
+import * as funcoes from '../funcoes';
+
+console.log(funcoes.soma(1,5));
+
+//console.log(soma(1,5));
+
+//console.log(soma(1,10));
+
+//console.log(sub(10,1));
+*/
+
+/*import  Usuario from '../function'; 
+import {idade} from '../function';*/
+import defoultUsuario,{ idade as idadeUsuario} from '../Modulo 2/function';
+
+defoultUsuario.info();
+
+console.log(idadeUsuario);
